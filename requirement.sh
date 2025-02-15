0f7d4516744cc6b60474bdecebf02dd3f76087a3
DDuttaGit <52165986+DDuttaGit@users.noreply.github.com>
2025-02-15 12:53:45 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -6,7 +6,9 @@ ollama serve
 ollama pull gemma2
 ollama pull llama3
 ollama pull mathstral
+ollama pull qwen2-math:7b
+ollama pull phi3:14b
 
-pip install ollama simpletransformers
+pip install ollama simpletransformers openai
 
 sudo apt-get install dvipng texlive-latex-extra texlive-fonts-recommended cm-super
